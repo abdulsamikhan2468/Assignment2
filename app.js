c30715d785e97e76e1786d006d9641cb23332507
Abdul Sami Khan <134823347+abdulsamikhan2468@users.noreply.github.com>
2024-03-02 17:01:51 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,84 @@
+//////////////      Assignment 2 //////////////////////////
+
+// Q1
+var username;
+
+// Q2
+var myname;
+myname="Abdul Sami Khan";
+
+// Q3
+
+var message = "Hello World";
+alert(message);
+
+
+// Q4
+
+var fullname="Jhone Doe";
+var age="15 years old";
+var course="Certified Mobile Application Development";
+
+alert(fullname);
+alert(age);
+alert(course);
+
+// Q6
+
+var email="abdulsamikhan36912@gmail.com";
+
+alert("My email address is "+ email)
+
+// Q7
+
+var BOOK
+BOOK="A smarter way to learn JavaScript."
+alert("I am trying to learn from the Book" + BOOK)
+
+// Q8
+
+document.write('Yah! I can write HTML content through Java script')
+
+// Q9
+
+var pic="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(pic);
+
+
+
+
+
+
+
+
+
+
+///////////////// Assignment 3 ///////////////////////
+
+// Q1
+
+var age=15;
+alert("I am "+age+" years old.")
+
+// Q2
+
+var times=14;
+alert("You have visited this site "+times+" times.")
+
+// Q3
+
+var birthyear="2003";
+document.write("My birth year is "+ birthyear)
+document.write("Data type of my declared variable is number.")
+
+
+// Q4
+
+var vname="Johne Doe";
+var ptitle="T-Shirt(s)";
+var quan=5;
+document.write(vname+" ordered "+quan+ptitle+" on XYZ clothing store.")
+
+
+
+
